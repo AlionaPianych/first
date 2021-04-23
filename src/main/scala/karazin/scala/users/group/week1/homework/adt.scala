77fa801d930704c48cec4992916c5855bf73fa90
AlionaPianych <alionaqwer09@gmail.com>
2021-04-23 19:38:07 +0300

    week-1

--- src/main/scala/karazin/scala/users/group/week1/homework/adt.scala
+++ src/main/scala/karazin/scala/users/group/week1/homework/adt.scala
@@ -1,53 +1,34 @@
 package karazin.scala.users.group.week1.homework
 
-/* 
-  Custom implementation of Option (Maybe monad in Haskell)
-  Implemented via Scala 3 way for Algebraic Data Types (ADT)
-  
-  Resources:
-  * https://en.wikipedia.org/wiki/Algebraic_data_type
-  * https://docs.scala-lang.org/scala3/book/types-adts-gadts.html
-*/
+import scala.util.control.NonFatal
 
 object adt:
-  
-  enum ErrorOr[+V]:
-    
-    // Added to make it compilable. Remove it.
-    case DummyCase
-    
-    /* 
-      Two case must be defined: 
-      * a case for a regular value
-      * a case for an error (it should contain an actual throwable)
-     */
-  
-    /* 
-      The method is used for defining execution pipelines
-      Provide a type parameter, an argument and a result type
-      
-      Make sure that in case of failing the method with exception
-      no exception is thrown but the case for an error is returned
-    */ 
-    def flatMap = ???
 
-    /* 
-      The method is used for changing the internal object
-      Provide a type parameter, an argument and a result type
-      
-      Make sure that in case of failing the method with exception
-      no exception is thrown but the case for an error is returned
-     */
-    def map = ???
-      
-  // Companion object to define constructor
+  enum ErrorOr[+V]:
+
+    case Some(x: V) extends ErrorOr[V]// Если любой тип кроме Throwable 
+
+    case SomeError(x: Throwable) extends ErrorOr[V]// если  какае-то ошибка 
+
+    def flatMap[Q](f: V ⇒ ErrorOr[Q]): ErrorOr[Q] = {
+      this match
+        case ErrorOr.SomeError(q) ⇒ ErrorOr.SomeError(q)//если ошибка сразу то ее и возвращаем 
+        case ErrorOr.Some(v) ⇒ try {
+          f(v)
+        } catch {
+          case NonFatal(e) ⇒ ErrorOr.SomeError(e)
+        }
+    }
+
+    def map[Q](f: V ⇒ Q): ErrorOr[Q] =
+      this match
+        case ErrorOr.SomeError(t) ⇒ ErrorOr.SomeError(t)
+        case ErrorOr.Some(v) ⇒ try {
+          ErrorOr.Some(f(v))
+        } catch {
+          case NonFatal(e) ⇒ ErrorOr.SomeError(e)
+        }
+
   object ErrorOr:
-    /* 
-      Provide a type parameter, an argument and a result type
-      
-      Make sure that in case of failing the method with exception
-      no exception is thrown but the case for an error is returned
-    */
-    def apply = ???
-      
-  
+    def apply[V](v: V): ErrorOr[V] =
+      if v == null then ErrorOr.SomeError(throw new Exception(s"${v}")) else ErrorOr.Some(v) 
