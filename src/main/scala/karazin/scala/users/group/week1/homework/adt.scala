e12bea09fc8b916db50f2bbb7a69e0e6ee9e56a1
AlionaPianych <alionaqwer09@gmail.com>
2021-04-23 20:56:51 +0300

    week-1

--- src/main/scala/karazin/scala/users/group/week1/homework/adt.scala
+++ src/main/scala/karazin/scala/users/group/week1/homework/adt.scala
@@ -6,17 +6,17 @@ object adt:
 
   enum ErrorOr[+V]:
 
-    case Some(x: V) extends ErrorOr[V]// Если любой тип кроме Throwable 
+    case Some(x: V) extends ErrorOr[V]
 
-    case SomeError(x: Throwable) extends ErrorOr[V]// если  какае-то ошибка 
+    case SomeError(x: Throwable) extends ErrorOr[V]
 
     def flatMap[Q](f: V ⇒ ErrorOr[Q]): ErrorOr[Q] = {
       this match
-        case ErrorOr.SomeError(q) ⇒ ErrorOr.SomeError(q)//если ошибка сразу то ее и возвращаем 
+        case ErrorOr.SomeError(q) ⇒ ErrorOr.SomeError(q)
         case ErrorOr.Some(v) ⇒ try {
           f(v)
         } catch {
-          case NonFatal(e) ⇒ ErrorOr.SomeError(e)
+          case e: Exception ⇒ ErrorOr.SomeError(e)
         }
     }
 
@@ -31,4 +31,4 @@ object adt:
 
   object ErrorOr:
     def apply[V](v: V): ErrorOr[V] =
-      if v == null then ErrorOr.SomeError(throw new Exception(s"${v}")) else ErrorOr.Some(v) 
+      if v == null then ErrorOr.SomeError(throw new Exception(s"${v}")) else ErrorOr.Some(v)
